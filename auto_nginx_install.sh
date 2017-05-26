fc70eedac932d65bea4bb980f5d05be9771c1ff6
N1neSun <917549681@qq.com>
2017-05-26 16:29:07 +0800

    Update auto_nginx_install.sh

--- auto_nginx_install.sh
+++ auto_nginx_install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+read -p "Input the nginx version:" version
+echo "The version is $version"
+isOK=$(curl -I http://nginx.org/download/nginx-$version.tar.gz | grep 200)
+if[ "$isOk" == "" ]; then
+read -p "The version is not exist,re-enter:" version
+fi
 yum install -y pcre pcre-devel
 yum install -y zlib zlib-devel
 yum install -y openssl openssl-devel
 yum install -y gcc
 yum install -y gcc-c++
 yum install -y wget
-wget -c http://nginx.org/download/nginx-1.10.3.tar.gz
-tar -zvxf nginx-1.10.3.tar.gz
-cd nginx-1.10.3
+wget -c http://nginx.org/download/nginx-$version.tar.gz
+tar -zvxf nginx-$version.tar.gz
+cd nginx-$version
 ./configure --prefix=/usr/local/nginx --with-http_stub_status_module && make && make install
