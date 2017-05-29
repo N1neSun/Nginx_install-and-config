ff4d9390adc6a40fbdd9a4161dbd75ee94670a7d
N1neSun <917549681@qq.com>
2017-05-29 17:33:56 +0800

    Update auto_nginx_install.sh
    更新用户自定义安装路径

--- auto_nginx_install.sh
+++ auto_nginx_install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 read -p "Input the nginx version:" version
 echo "The version is $version"
-read -p "Input the user and group:" user group
 isOK=$(curl -I http://nginx.org/download/nginx-$version.tar.gz | grep 200)
 while [ "$isOK" == "" ];
 do
 read -p "The version is not exist,re-enter:" version
 isOK=$(curl -I http://nginx.org/download/nginx-$version.tar.gz | grep 200)
 done
+read -p "Input the user and group:" user group
+read -p "Nginx will install in /usr/local/nginx. agree?(Y/N):" flag
+if [ "$flag" == "N" ] || [ "$flag" == "n" ]; then
+read -p "Input where the nginx install(e.x.:/usr/local/nginx):" Nginx_path
+else
+Nginx_path="/usr/local/nginx"
+fi
 yum install -y pcre pcre-devel
 yum install -y zlib zlib-devel
 yum install -y openssl openssl-devel
@@ -19,4 +25,4 @@ tar -zvxf nginx-$version.tar.gz
 cd nginx-$version
 groupadd $group
 useradd -g $user $user
-./configure --user=$user --group=$group --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module && make && make install
+./configure --user=$user --group=$group --prefix=$Nginx_path --with-http_stub_status_module --with-http_ssl_module && make && make install
